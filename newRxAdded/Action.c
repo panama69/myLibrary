1c06bc45be483f974c007e72ffc36192f4632e83
Dave Flynn <github@flynnshome.com>
2021-01-19 08:12:28 -0800

    newRxAdded script

---
+++ newRxAdded/Action.c
@@ -0,0 +1,7 @@
+Action()
+{
+	// scripted added to the 'myLibrary' git project.
+	// This approach will need to ignore the built in integrations of Vugen and just use native
+	// cli or GitHub Desktop to perform commits, pull, push, etc.
+	return 0;
+}
